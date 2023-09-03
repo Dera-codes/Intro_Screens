034a1dd93fa300839dffbcf00d07983a04509ea8
MAC <mac@MACs-MacBook-Pro.local>
2023-09-03 07:13:41 +0100

    app tweaks

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intro_screen/intro_screens/intro_page1.dart';
+import 'package:intro_screen/intro_screens/intro_page2.dart';
+import 'package:intro_screen/intro_screens/intro_page3.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -8,18 +12,29 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  // controller to keep track of which page we,re on
+  PageController _controller = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
+      body: Stack(
         children: [
-          Container(
-            color: Colors.red,
+          // page view
+          PageView(
+            controller: _controller,
+            children: [
+            IntroPage1(),
+            IntroPage2(),
+            IntroPage3(),
+            ],
           ),
+          // dot indicator
 
-          Container(color: Colors.blue,),
-
-          Container(color: Colors.green,),
+          Container(
+            alignment: Alignment(0, 0.75),
+            child: SmoothPageIndicator(controller: _controller, count: 3),
+          ),
         ],
       ),
     );
